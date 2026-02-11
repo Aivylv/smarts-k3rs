4fcb1e40ac93024210cfc131d1fbc67a7468725c
aivylv <lyvialarasati@gmail.com>
2026-02-11 07:55:11 +0700

    ubah tombol detail

--- resources/views/livewire/apar/apar-index.blade.php
+++ resources/views/livewire/apar/apar-index.blade.php
@@ -164,23 +164,27 @@
                                 </span>
                             </td>
                             <td>
-                                <div class="flex items-center justify-center gap-1">
-                                    <a href="{{ route('apar.show', $apar->id_apar) }}" class="btn btn-ghost btn-sm btn-square" title="Detail">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <div class="flex items-center justify-center gap-2">
+                                    <a href="{{ route('apar.show', $apar->id_apar) }}" class="btn btn-sm btn-info text-white font-normal">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                         </svg>
+                                        Detail
                                     </a>
-                                    <a href="{{ route('apar.edit', $apar->id_apar) }}" class="btn btn-ghost btn-sm btn-square" title="Edit">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                        </svg>
-                                    </a>
-                                    <button wire:click="confirmDelete('{{ $apar->id_apar }}')" class="btn btn-ghost btn-sm btn-square text-error" title="Hapus">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                        </svg>
-                                    </button>
+
+                                    <div class="join">
+                                        <a href="{{ route('apar.edit', $apar->id_apar) }}" class="btn btn-ghost btn-sm btn-square join-item" title="Edit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                            </svg>
+                                        </a>
+                                        <button wire:click="confirmDelete('{{ $apar->id_apar }}')" class="btn btn-ghost btn-sm btn-square text-error join-item" title="Hapus">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
                             </td>
                         </tr>
